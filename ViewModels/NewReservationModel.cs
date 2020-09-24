91c11499a20093ad18da4d6156ccacad6cb5fd88
Kazkarro <maxime.paletou@icloud.com>
2020-09-24 11:23:41 +0200

    Implémentation nouvelle réservation

---
+++ ViewModels/NewReservationModel.cs
@@ -0,0 +1,45 @@
+﻿using ProjetRESOTEL.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ProjetRESOTEL.ViewModels
+{
+    public class NewReservationModel
+    {
+        #region Commands
+
+        private RelayCommand nouvelleChambre;
+        public StackPanel stack;
+
+        public ICommand NouvelleChambre
+        {
+            get
+            {
+                if (nouvelleChambre == null)
+                {
+                    nouvelleChambre = new RelayCommand(ajoutChambre);
+                }
+                return nouvelleChambre;
+            }
+        }
+
+        public void ajoutChambre()
+        {
+            ComboBox cmb = new ComboBox(); // Création d'un nouveau combo box
+            cmb.Items.Add("Chambre Individuelle");
+            cmb.Items.Add("Chambre Double");
+            cmb.Items.Add("Chambre 3 personnes");
+            cmb.Items.Add("Chambre 4 personnes");
+            cmb.Items.Add("Chambre 6 personnes");
+            cmb.Items.Add("Chambre Double + Bébé");
+
+            stack.Children.Add(cmb); // Ajout du combo box dans le stackpanel
+        }
+        #endregion
+    }
+}
--- Views/ucNewReservation.xaml.cs
+++ Views/ucNewReservation.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
 
 namespace ProjetRESOTEL.Views
 {
@@ -27,7 +28,40 @@ namespace ProjetRESOTEL.Views
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+        }
 
+        private void addRoom_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBox cmb = new ComboBox(); // Création d'un nouveau combo box
+            cmb.Items.Add("Chambre Individuelle");
+            cmb.Items.Add("Chambre Double");
+            cmb.Items.Add("Chambre 3 personnes");
+            cmb.Items.Add("Chambre 4 personnes");
+            cmb.Items.Add("Chambre 6 personnes");
+            cmb.Items.Add("Chambre Double + Bébé");
+
+            panelComboBox.Children.Add(cmb); // Ajout du combo box dans le stackpanel
+        }
+
+        private void checkCardio_Checked(object sender, RoutedEventArgs e)
+        {
+            SingleUpDown sgl = new SingleUpDown();
+            sgl.Value = 1;
+            sgl.Increment = 1;
+            SolidColorBrush transp = new SolidColorBrush();
+            sgl.Background = transp;
+            sgl.BorderBrush = transp;
+            SolidColorBrush white = new SolidColorBrush(Colors.White);
+            sgl.Foreground = white;
+            optCardio.Children.Add(sgl);
+        }
+
+        private void checkCardio_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (optCardio.Children.Count > 0)
+            {
+                optCardio.Children.RemoveAt(optCardio.Children.Count - 1);
+            }
         }
     }
 }
