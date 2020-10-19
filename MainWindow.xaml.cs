348e176fb981bc06ecf9a2f993f99db5d90f5b81
alexisBes <32544682+alexisBes@users.noreply.github.com>
2020-10-19 16:26:31 +0200

    - mise en place de l'affichage pour le calendrier journalier

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -55,5 +55,14 @@ namespace ProjetRESOTEL
             MainTab.Items.Add(_tabUserPage);
             MainTab.Items.Refresh();
         }
+        private void btnDayCalendar_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem _tabUserPage;
+            MainTab.Items.Clear();
+            var userControle = new ucPlanningJour();
+            _tabUserPage = new TabItem { Content = userControle };
+            MainTab.Items.Add(_tabUserPage);
+            MainTab.Items.Refresh();
+        }
     }
 }
