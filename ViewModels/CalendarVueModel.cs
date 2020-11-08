53d524d4b012418bb3e1d665d4d09099d77d8535
alexis besonnat <bessonnatalexis@gmail.com>
2020-11-08 15:37:25 +0100

    - ajout des fichier necessaire au CRUD

---
+++ ViewModels/CalendarVueModel.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using ProjetRESOTEL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetRESOTEL.ViewModels
+{
+    class CalendarVueModel
+    {
+        public List<Repas> loadDailyMeal()
+        {
+            List<Repas> lst;
+            using(Entities.AppContext context = new Entities.AppContext())
+            {
+                lst = context.Repas.ToList();
+            }
+            return lst;
+        }
+    }
+}
