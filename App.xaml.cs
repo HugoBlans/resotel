53b191ffc727ce5714aaebc3aa83e4b21e79a5ff
Hugo Blans <hugo.blans@gmail.com>
2020-11-19 17:14:31 +0100

    update gestion Utilisateurs

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,29 @@ namespace ProjetRESOTEL
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+
+            var principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            var domaine = System.Environment.UserDomainName;
+
+            if (principal.IsInRole(domaine + "\\ResotelHote"))
+            {
+                App.Current.Properties["isStandardiste"] = true;
+                App.Current.Properties["isHygiene"] = true;
+                App.Current.Properties["isRestauration"] = true;
+            }
+            else
+            {
+                App.Current.Properties["isStandardiste"] = principal.IsInRole(domaine + "\\ResotelStandardiste");
+                App.Current.Properties["isHygiene"] = principal.IsInRole(domaine + "\\ResotelHygiene");
+                App.Current.Properties["isRestauration"] = principal.IsInRole(domaine + "\\ResotelRestauration");
+            }
+
+            // For test only
+            App.Current.Properties["isStandardiste"] = true;
+            App.Current.Properties["isHygiene"] = true;
+            App.Current.Properties["isRestauration"] = true;
+        }
     }
 }
---
+++ Converters/RoleToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ProjetRESOTEL.Converters
+{
+    class RoleToVisibilityConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isAuthorized = (bool)value;
+            if (isAuthorized)
+            {
+                return "Visible";
+            }
+            return "Collapsed";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
